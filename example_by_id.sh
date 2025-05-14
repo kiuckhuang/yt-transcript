8ccf252ecc46029e90a8b806bccab1c699c29d28
CK Huang <ckhuang@life.hkbu.edu.hk>
2025-05-14 11:28:33 -0700

    add example by using YT video ID

---
+++ example_by_id.sh
@@ -0,0 +1,74 @@
+#!/opt/homebrew/bin/bash
+
+# sample URL:
+# https://www.youtube.com/watch?v=vT1_kGcNf7A
+YT_URL=$1
+video_id=''
+
+
+# Check if input is a valid YouTube video ID (11 chars: letters, numbers, _ or -)
+if [[ "$YT_URL" =~ ^[A-Za-z0-9_-]{11}$ ]]; then
+    video_id=${YT_URL}
+fi
+
+# Regex to extract video ID from various YouTube URL formats
+regex='(youtu\.be/|youtube\.com/(watch\?v=|embed/|v/|shorts/|shorts\?v=|.*[?&]v=))([A-Za-z0-9_-]{11})'
+
+if [[ $YT_URL =~ $regex ]]; then
+    video_id="${BASH_REMATCH[3]}"
+    #echo "Video ID: $video_id"
+fi
+
+if [ -z "${video_id}" ]; then
+    echo "Input is not a valid YouTube video URL or ID."
+    echo "$0 YUTUBE_URL"
+    exit 1;
+fi
+
+
+MODELS=./models
+AUDIOS=./audios
+TRANSCRIPTS=./transcripts
+
+mkdir -p "${MODELS}"
+mkdir -p "${AUDIOS}"
+mkdir -p "${TRANSCRIPTS}"
+
+filename=${video_id}
+
+TRANSCRIPT_FILE=${TRANSCRIPTS}/${filename}
+
+MP3=${AUDIOS}/${filename}.mp3
+
+WHISPER_MODEL=${MODELS}/whisper-large-v3-cantonese.bf16.bin
+
+# check if whisper model file exist
+if [ ! -f "${WHISPER_MODEL}" ]; then
+    echo "Whisper model file not found: ${WHISPER_MODEL}"
+    exit 1;
+fi
+
+# 1. Use `yt-dlp` to download audio content
+if [ -f "${MP3}" ]; then
+    echo "Re-use existing ${MP3}"
+else
+    yt-dlp -f 'ba[acodec^=mp3]/ba/b' -x --audio-format mp3 -o ${MP3} "${YT_URL}"
+
+    if [[ $? -ne 0 ]]; then
+        echo "$0 YUTUBE_URL"
+        exit 1;
+    fi
+fi
+
+# 2. Process audio files with `whisper-cpp` using appropriate models
+if [ -f "${TRANSCRIPT_FILE}.lrc" ]; then
+    echo "Re-use existing ${TRANSCRIPT_FILE}"
+else
+    whisper-cli -m ${WHISPER_MODEL} -l auto ${MP3} -olrc -fa -sns --output-file ${TRANSCRIPT_FILE}
+fi
+
+# 3. Use `llm` and `ollama` for language model inference
+cat ${TRANSCRIPT_FILE}.lrc | llm -m qwen3_4b -s "show with Traditional Hong Kong Chinese, list the items discuss in the video transcript, in point form, make summary /no_think"
+
+# use Google Gemini free model (non-local)
+# cat ${TRANSCRIPT_FILE}.lrc | llm -m gemini-2.0-flash -s "show with Traditional Hong Kong Chinese, list the items discuss in the video transcript, in point form, make summary"
